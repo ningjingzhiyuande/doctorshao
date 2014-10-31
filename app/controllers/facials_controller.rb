57e6037fc63bf6f7b012e2184b219322776b8b9a
shiguodong <oreatial@gmail.com>
2014-10-31 16:24:00 +0800

    add

--- app/controllers/facials_controller.rb
+++ app/controllers/facials_controller.rb
@@ -3,6 +3,8 @@ class FacialsController < ApplicationController
   end
 
   def show
+
      @project = Project.find params[:id]
+     @parent_id = @project.root? ? @project.id : @project.root.id
   end
 end
